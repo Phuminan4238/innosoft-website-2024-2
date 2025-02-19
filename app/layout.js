7d96d4f652254ad4c0e74406e4e1d29038f3b6ea
Phuminan4238 <phuminan@live.com>
2025-02-19 15:23:51 +0700

    feat: add UserWay accessibility widget script to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,14 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* UserWay Accessibility Widget */}
+        <script
+          src="https://cdn.userway.org/widget.js"
+          data-account="kmoNJ8LbJN"
+          async
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   );
