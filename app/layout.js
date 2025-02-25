1b96f870f1b9675a1482856e97addbae8c9cdb2e
Phuminan4238 <phuminan@live.com>
2025-02-25 13:51:31 +0700

    feat: add Google Analytics (gtag.js) and UserWay accessibility script to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,30 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* UserWay Accessibility Widget */}
+        <script
+          src="https://cdn.userway.org/widget.js"
+          data-account="kmoNJ8LbJN"
+          async
+        ></script>
+
+        {/* Google Tag Manager */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-EFZNGWM7VT"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EFZNGWM7VT');
+          `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
